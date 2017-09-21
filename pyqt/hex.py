8333994eaa2d590f2594aa976b0762dc7158b2b6
jmfernandes <joshfernandes@mac.com>
2017-09-21 15:52:56 -0700

    automated making several tiles

--- pyqt/hex.py
+++ pyqt/hex.py
@@ -19,7 +19,7 @@ import math
 from PyQt5.QtCore import QObject, QPointF
 from PyQt5.QtGui import QFont, QPen, QColor, QBrush, QPolygonF, QPainter
 from PyQt5.QtWidgets import QApplication, QToolTip, QWidget, QLabel,\
-                            QPushButton, QDialog
+                            QPushButton, QDialog,QDesktopWidget
 
 Point = collections.namedtuple('Point', 'x y')
 
@@ -50,32 +50,11 @@ class Hex:
         angle_deg = 60*i
         return Point(center[0]+size*math.cos(math.radians(angle_deg)),
                      center[1]+size*math.sin(math.radians(angle_deg)))
-
-# h=Hex(1,2,3)
-# print(h)
-# print(Hex(1,-3,2)+Hex(3,-7,4))
-# print(Hex(1,-3,2)-Hex(3,-7,4))
-# print(h.size)
-# for i in range(6):
-#     print(h.hex_corner((15,17.3205),10,i))
-h = Hex(0,0)
-print(h)
-print(h.center.x,h.center.y)
-print(h.width)
-print(h.height)
-print(h.hex_corner(h.center,h.size,3).x)
-
-width = 50*2*3/4
-height = -math.sqrt(3)/4*50*2
-
-# print(width,height)
-# print('------')
-# q=3
-# r=-1
-# print(q*width,-q*height-(r*height*2))
-# q=1
-# r=0
-# print(q*width,-q*height-(r*height*2))
+size=1
+for i in range(-size,size+1):
+    for j in range(-size,size+1):
+        if (abs(i+j) <= size):
+            print(i,j)
 
 class window(QDialog):
 
@@ -87,41 +66,39 @@ class window(QDialog):
 
     def initUI(self):
 
-        self.resize(500,500)
+        self.windowSize = 500
+        self.resize(self.windowSize,self.windowSize)
         self.center
-        self.setWindowTitle("PyQt")
+        self.setWindowTitle("PyQt - Hex Board")
 
     def initBoard(self):
         self.pen = QPen(QColor(0,0,0))                      # set lineColor
         self.pen.setWidth(3)                                            # set lineWidth
         self.brush = QBrush(QColor(255,255,255,255))        # set fillColor
-        self.polygon = self.createPoly(Hex(0,0))                         # polygon with n points, radius, angle of the first point
-        self.polygon2 = self.createPoly(Hex(1,0))
-        self.polygon3 = self.createPoly(Hex(0,1))
-        self.polygon4 = self.createPoly(Hex(1,1))
+        self.radius = 2
+        # self.polygon = self.createPoly(Hex(0,0))                         # polygon with n points, radius, angle of the first point
+        # self.polygon2 = self.createPoly(Hex(1,0))
+        # self.polygon3 = self.createPoly(Hex(0,1))
+        # self.polygon4 = self.createPoly(Hex(-1,0))
+        # self.polygon5 = self.createPoly(Hex(0,-1))
+        # self.polygon6 = self.createPoly(Hex(-1,1))
+        # self.polygon7 = self.createPoly(Hex(1,-1))
 
     def initShow(self):
         self.show()
 
     def center(self):
         qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
+        cp = availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
     def createPoly(self,item):
-        polygon = QPolygonF()                                                    # angle per step
-        for i in range(6):                                              # add the points of polygon
-            # angle_deg = 60*i
-            # size = item.size
-            # x = size*math.cos(math.radians(angle_deg))
-            # y = size*math.sin(math.radians(angle_deg))
-            # center = (item.center.x+250,250-item.center.y)
-            cords = item.hex_corner(item.center,item.size,i)
-            # print(y,cords.y-item.center.y)
-            # print(center[1]+y,250-2*item.center.y+cords.y)
-            # print(center[0]+x,250+cords.x)
-            polygon.append(QPointF(250+cords.x,250-2*item.center.y+cords.y))
+        polygon = QPolygonF()
+        for i in range(6): # add the points of polygon
+            coords = item.hex_corner(item.center,item.size,i)
+            polygon.append(QPointF(self.windowSize/2+coords.x,
+                                   self.windowSize/2-2*item.center.y+coords.y))
 
         return polygon
 
@@ -129,10 +106,17 @@ class window(QDialog):
         painter = QPainter(self)
         painter.setPen(self.pen)
         painter.setBrush(self.brush)
-        painter.drawPolygon(self.polygon)
-        painter.drawPolygon(self.polygon2)
-        painter.drawPolygon(self.polygon3)
-        painter.drawPolygon(self.polygon4)
+        for i in range(-self.radius,self.radius+1):
+            for j in range(-self.radius,self.radius+1):
+                if (abs(i+j) <= self.radius):
+                    polygon = self.createPoly(Hex(i,j))
+                    painter.drawPolygon(polygon)
+        # painter.drawPolygon(self.polygon2)
+        # painter.drawPolygon(self.polygon3)
+        # painter.drawPolygon(self.polygon4)
+        # painter.drawPolygon(self.polygon5)
+        # painter.drawPolygon(self.polygon6)
+        # painter.drawPolygon(self.polygon7)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
