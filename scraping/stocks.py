9712a816845bdb82c04dd255a5e83101aeacf1c2
jmfernandes <joshfernandes@mac.com>
2018-01-10 13:35:05 -0800

    Added web scraping stocks

---
+++ scraping/stocks.py
@@ -0,0 +1,32 @@
+########################################
+#
+# stocks.py
+#
+# Description:
+#
+#
+# Author: Josh Fernandes
+#
+# Created: Jan 10, 2018
+#
+# Updated:
+#
+#
+########################################
+from collections import OrderedDict
+
+holdings = {
+            'BOTZ': {'shares' : 2, 'sector' : 'technology'},
+            'SLIM': {'shares' : 2, 'sector' : 'health'}
+            }
+print(holdings)
+print(holdings['BOTZ'])
+print(holdings['BOTZ']['shares'])
+print(type(holdings['BOTZ']['shares']))
+print(type(holdings['BOTZ']['sector']))
+print(holdings.keys())
+
+holdings['CORT'] = {'shares' : 2, 'sector' : 'health'}
+
+for key,value in holdings.items():
+    print(key,' - ', value)
